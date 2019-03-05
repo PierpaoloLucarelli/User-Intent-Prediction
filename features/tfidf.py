5bd9a5a29d137201736d73eb4849fa4883841da4
Pierpaolo <plucarelli.94@gmail.com>
2019-03-05 15:52:57 +0100

    pickle

--- features/tfidf.py
+++ features/tfidf.py
@@ -1,14 +1,16 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import math
 from sklearn.feature_selection import chi2
+import pickle 
 
+def save_tfidf(datafile, savename):
 
-corpus = []
-positions = []
-labels = []
-cosines = []
-
-def save_tfidf(datafile):
+    corpus = []
+    positions = []
+    labels = []
+    cosines = []
+    print("one call")
+    print(datafile)
     with open(datafile) as conversations:
         i = 0
         j = 0
@@ -30,6 +32,8 @@ def save_tfidf(datafile):
         initial_utterance = X[initial_document_pos]
         cos_sim = cosine_similarity(utterance,initial_utterance)
         cosines.append(cos_sim)
+    with open(savename + '.pkl','wb') as f:
+        pickle.dump(cosines, f)
     return cosines
 
 def cosine_similarity(vector1, vector2):
@@ -37,5 +41,5 @@ def cosine_similarity(vector1, vector2):
     magnitude = math.sqrt(sum([val**2 for val in vector1])) * math.sqrt(sum([val**2 for val in vector2]))
     if not magnitude:
         return 0
-    return dot_product/magnitude
+    return round(dot_product/magnitude, 3)
 
